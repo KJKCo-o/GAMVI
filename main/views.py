de92c65cded6c04ab67206f6c5655277e1dc99b2
WooYong Kim <wykim970@gmail.com>
2022-01-24 01:26:34 +0900

    Feat: 메인 페이지 및 일기 기록 페이지 추가
    css 추가 수정 필요

--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'main/home.html')
+
+def voice(request):
+    return render(request, 'main/voice_record.html')
