5df05f2691dde226a36a207f0cfb70b0cb83c6ec
sua1223 <70890638+sua1223@users.noreply.github.com>
2022-01-23 15:51:55 +0900

    Chore: 데이터베이스 연동

--- gamvi/settings.py
+++ gamvi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pr0q^qhh!1obn&r15*)r$#_u3p3*w8d2+x!^=c9zqgn(-ny_$i'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,9 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main',
+    'rest_framework', # DRF
+    'corsheaders', # django-cors-headers
 ]
 
+# rest_framework 추가
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # corsheader 추가
+    'django.middleware.common.CommonMiddleware', # corsheader 추가
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +63,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://ec2-15-164-104-128.ap-northeast-2.compute.amazonaws.com:8080/'
+)
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'gamvi.urls'
 
@@ -74,13 +96,7 @@ WSGI_APPLICATION = 'gamvi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = config.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- main/models.py
+++ main/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Calendar(models.Model):
+    calender_id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+    date = models.DateTimeField()
+    emotion = models.CharField(max_length=12)
+
+    class Meta:
+        managed = False
+        db_table = 'CALENDAR'
+
+
+class Detail(models.Model):
+    detail = models.OneToOneField(Calendar, models.DO_NOTHING, primary_key=True)
+    sentence = models.TextField()
+    voice = models.TextField()
+    enjoyment = models.FloatField()
+    sadness = models.FloatField()
+    anger = models.FloatField()
+    surprise = models.FloatField()
+    disgust = models.FloatField()
+    fear = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'DETAIL'
+
+
+class User(models.Model):
+    user_id = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=10)
+    age = models.IntegerField(blank=True, null=True)
+    sex = models.CharField(max_length=7, blank=True, null=True)
+    contact = models.CharField(max_length=11, blank=True, null=True)
+    password = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'USER'
