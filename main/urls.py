84772111f939113abdd75449c084507ab2077d67
mincastle98 <alstjd98522@naver.com>
2022-01-24 16:08:44 +0900

    Feat: Calendar GET Method 구현
    사용자가 해당 기간에 작성한 감정일기의 감정값 반환

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('voice', views.voice, name='voice')
+    path('voice', views.voice, name='voice'),
+
+    # 캘린더
+    path('calendar', views.CalendarList.as_view())
 ]
